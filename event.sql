9d3cf201b7c1b664399229c6d852c6c50a172bcf
Tanushree Sarangi <tanushreesarangi29@gmail.com>
2020-06-24 13:27:41 -0400

    Updating the comments

--- event.sql
+++ event.sql
@@ -1,53 +1,44 @@
-Select * from event_log;
+--create the table to fill the event_log data with
 create table event_log_2
 (
 uuid character varying,	
-timestamp	integer,
+timestamp	character varying,
 session_id character varying,
 "group" character varying,	
 "action" character varying,	
-checkin	integer,
+checkin	character varying,
 page_id	character varying,	
-n_results integer,
-result_position	integer)
+n_results character varying,
+result_position	character varying)
 
-ALTER TABLE event_log_2
-ALTER COLUMN timestamp TYPE character varying
-
-ALTER TABLE event_log_2
-ALTER COLUMN n_results TYPE character varying
-
-select * from event_log_2 where action = 'searchResultPage' and n_results = '0' limit 10 "760bf89817ce4b08" "30"
+--import the csv data into this table.
 
 update event_log_2
 set n_results = null
 where n_results = 'NA'
 
-select Left("timestamp",8) from event_log_2 limit 10
---Assuming that the actions - visitPage and checkin will always have a result_position associated with them. Hence, removing the records
---where the action is VisitPage or checkin and the result_position is null
---Assuming that the timestamp will always have a numeric value. Hence, removing the records where the timestamp column has special characters or alpha numeric value.
---For zero yield we have to take in account both where the n_results are null and where the n_results are 0.
-select * from event_log_2 where result_position <> 'NA' limit 10
-
+--this CTE(Common Table Expression) counts the unique session_ids for searchResultPage for all dates and groups
 WITH CTE_searchResults as
 (
 select Left("timestamp",8) as datepart_timestamp, "group",count(distinct session_id) as total_searches from event_log_2 
 where action = 'searchResultPage' 
 group by "group", Left("timestamp",8)
 )
+--this CTE counts the unique session_ids for visitPage for all dates and groups
 , CTE_visitPages as
 (
 select Left("timestamp",8) as datepart_timestamp, "group",count(distinct session_id) as total_visits from event_log_2 
 where action = 'visitPage' and result_position <> 'NA'
 group by "group", Left("timestamp",8)
 )
+--this CTE counts the unique session_ids for searchResultPage which yielded 0 or null results, for all dates and groups
 ,CTE_zeroYield as
 (
 select Left("timestamp",8) as datepart_timestamp, "group",count(distinct session_id) as total_zeroyields from event_log_2 
 where action = 'searchResultPage' and (n_results = '0' or n_results is null)
 group by "group", Left("timestamp",8)
 )
+--count all the result_postions for all dates and sessions, ranking them according to their count (max count --> rank 1, 2nd most count -->rank 2...)
 ,CTE_count_resultPostion as
 (
 select Left("timestamp",8) as datepart_timestamp, "group", 
@@ -58,6 +49,9 @@ from event_log_2
 where action = 'visitPage' and result_position <> 'NA'
 group by "group", Left("timestamp",8), result_position
 )
+--Join all the CTEs together
+--left joining on the CTE_searchResults as the workflow starts from searching and the other actions cannot be performed without it
+--filtering only for the Rank 1 counts to get the max clicked resuklt position for everyday per group
 select a.datepart_timestamp, a."group", cast(total_visits*100/total_searches as float) as clickthroughrate, 
 (total_zeroyields*100/total_searches) as zeroyield,
 d.result_position
@@ -68,21 +62,13 @@ left join CTE_count_resultPostion d on a.datepart_timestamp = d.datepart_timesta
 where d.ranking = 1
 order by a.datepart_timestamp
 
-
-
-select * from event_log_2 where session_id = 'b549a21cf5f272e2'
-action = 'visitPage' and result_position = 'NA'
-
-select * from event_log_2 where Left("timestamp",8) ilike '%2.%'
-
-select Left("timestamp",8) as datepart_timestamp, "group",count(distinct session_id) as total_searches from event_log_2 
-where action = 'searchResultPage' 
-group by "group", Left("timestamp",8) order by Left("timestamp",8)
-
+--This cte gets the session_length
+--finding the max checkin time for each session and then summing it up (as one session can have different pages with different checkins)
+--grouping by the date, session and group
 with cte_max_checkin as
 (
 select max(cast(checkin as int)) as checkin, session_id,page_id, Left("timestamp",8) as datepart_timestamp from event_log_2 
-where action = 'checkin' --and session_id = '426b242692d66473'
+where action = 'checkin'
 group by session_id,page_id,Left("timestamp",8)
 order by Left("timestamp",8)
 )
@@ -91,6 +77,4 @@ from cte_max_checkin
 group by session_id, datepart_timestamp
 order by datepart_timestamp
 
-select * from event_log_2 where session_id = 'aa89be8089ff5694' order by timestamp asc
---"13cd6d70d0fa2b58"
 
